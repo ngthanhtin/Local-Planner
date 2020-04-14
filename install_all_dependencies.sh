2507a94352b26b9e5847ba77ce8a379ac63ad480
wym10407 <yangming.wu@uisee.com>
2020-04-15 00:02:30 +0800

    add submodule fast planner from hkust

--- install_all_dependencies.sh
+++ install_all_dependencies.sh
@@ -9,3 +9,9 @@ bash ${CURRENT_DIR}/install_map_server.sh
 
 echo "install ompl"
 bash ${CURRENT_DIR}/install_ompl.sh
+
+echo "install armadillo"
+bash ${CURRENT_DIR}/install_armadillo.sh
+
+echo "install nlopt"
+bash ${CURRENT_DIR}/install_nlopt.sh
